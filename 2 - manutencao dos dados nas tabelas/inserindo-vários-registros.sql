9b79b76a8a866b692e70aa2c64b7fcf3af6be7d6
Robeto Melo <rmello.filho@hotmail.com>
2023-12-18 17:39:50 -0300

    inserindo vários registros na tabela

---
+++ 2 - manutencao dos dados nas tabelas/inserindo-vários-registros.sql
@@ -0,0 +1,17 @@
+USE sucos;
+
+INSERT INTO tbproduto (
+	PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA
+) 
+VALUES (
+		'1040107', 'lIGHT - 350 ML - Melância', 'Lata', '350 ml', 'Melância', 4.56
+    );
+    
+INSERT INTO tbproduto (
+	PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA
+) 
+VALUES (
+		'1040107', 'lIGHT - 350 ML - Melância', 'Lata', '350 ml', 'Melância', 4.56
+    );
+
+SELECT * FROM sucos.tbproduto;

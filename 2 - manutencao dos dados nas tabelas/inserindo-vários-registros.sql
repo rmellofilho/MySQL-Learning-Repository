dad3b84815c4de3c583d31c38036179ddc979de6
Robeto Melo <rmello.filho@hotmail.com>
2023-12-18 18:02:31 -0300

    inserindo vários registros

--- 2 - manutencao dos dados nas tabelas/inserindo-vários-registros.sql
+++ 2 - manutencao dos dados nas tabelas/inserindo-vários-registros.sql
@@ -4,14 +4,21 @@ INSERT INTO tbproduto (
 	PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA
 ) 
 VALUES (
-		'1040107', 'lIGHT - 350 ML - Melância', 'Lata', '350 ml', 'Melância', 4.56
+		'1037797', 'Clean - 2 Litros Laranja', 'PET', '2 Litros', 'Laranja', 16.01
     );
     
 INSERT INTO tbproduto (
 	PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA
 ) 
 VALUES (
-		'1040107', 'lIGHT - 350 ML - Melância', 'Lata', '350 ml', 'Melância', 4.56
+		'1000889', 'Sabor da Montanha - 700 ml - Uva', 'Garrafa', '700 ml', 'Uva', 6.31
+    );
+
+INSERT INTO tbproduto (
+	PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA
+) 
+VALUES (
+		'1004327', 'Videira do Campo - 1,5 Ltiros - Melância', 'PET', '1,5 Litros', 'Melância', 19.51
     );
 
 SELECT * FROM sucos.tbproduto;

b9e8e9654232c60878ee15504df0d272a3e7e0a6
Robeto Melo <rmello.filho@hotmail.com>
2023-12-20 16:26:30 -0300

    selecionando um vendedor

---
+++ 3 - Consultando os dados/05 - selecionando um vendedor.sql
@@ -0,0 +1,11 @@
+
+-- Consultar dados de todos vendedores Cláudia Morais
+SELECT * FROM tbvendedores;
+
+-- Consultar dados de Cláudia Morais da Matricula '00236'
+SELECT * FROM tbvendedores
+WHERE MATRICULA = '00236';
+
+-- Consultar dados de Cláudia Morais da Matricula '00236'
+SELECT * FROM tbvendedores
+WHERE NOME = 'Cláudia Morais';
